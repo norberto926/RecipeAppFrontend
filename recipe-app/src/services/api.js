ddd63cd123bb04ae410f6fe02d34e0f58ab495fb
Norbert Gniadek <norbert.gniadek@gmail.com>
2024-08-05 15:23:22 +0200

    changing api url and adding toml file

--- recipe-app/src/services/api.js
+++ recipe-app/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api'
+const API_BASE_URL = 'https://recipeapp-a62p.onrender.com/api'
 
 export const fetchRecipes = async () => {
     const response = await fetch(`${API_BASE_URL}/recipes/`)
